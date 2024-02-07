55f087f8092286fb499bb0c26d76e85911173dea
Dhanukasanjna <158571832+Dhanukasanjna@users.noreply.github.com>
2024-02-08 01:04:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94763615626', 'D S D D H A N U ', true],
+  ['6281255369012', 'D S D D H A N U ', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'D S D D H A N U -ʙᴏᴛ'
+global.premium = 'false'
+global.packname = 'D S D D H A N U ┃ᴮᴼᵀ' 
+global.author = '@D S D D H A N U ' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
